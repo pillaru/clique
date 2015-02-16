91cc811c6f62f0dbfdbd0ea7c84fbfb043114e1d
Chekkan <harish_babu@outlook.com>
2015-02-16 09:33:07 +0000

    added home page route for admin app

--- core/server/index.js
+++ core/server/index.js
@@ -2,11 +2,18 @@ var express = require('express');
 
 function init(options)
 {
-	var app = express();
+	var app = express(); // the main app
+	var admin = express(); // the admin app
+
+	admin.get('/', function(req, res){
+		res.send('Admin Homepage');
+	});
 
 	app.get('/', function(req, res){
-		res.send('clique');
+		res.send('Clique Homepage');
 	});
+
+	app.use('/admin', admin);
 
 	var server = app.listen(3000, function(){
 		var host = server.address().address;
