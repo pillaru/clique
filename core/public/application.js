9ed72e12630aecfea35ccf4481dec7f7c6700d78
Chekkan <harish_babu@outlook.com>
2015-02-19 23:41:37 +0000

    styling setup view

--- core/public/application.js
+++ core/public/application.js
@@ -2,16 +2,15 @@
 
 angular.module('clique', ['ngRoute']);
 
-angular.module('clique').config(['$routeProvider', '$locationProvider',
-  function($routeProvider, $locationProvider) {
+angular.module('clique').config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.
       when('/admin/setup', {
         templateUrl: 'modules/admin/views/setup.client.view.html',
         controller: 'SetupController'
       });
       $locationProvider.html5Mode(true);
-  }]);
+}]);
 
 angular.element(document).ready(function(){
 	angular.bootstrap(document, ['clique']);
-})
+});
