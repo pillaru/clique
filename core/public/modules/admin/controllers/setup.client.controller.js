80c5887111ecbd58248be34267d457154e309323
Harish Babu <harish_babu@outlook.com>
2015-02-20 19:29:54 +0000

    setup form client side validation completed

--- core/public/modules/admin/controllers/setup.client.controller.js
+++ core/public/modules/admin/controllers/setup.client.controller.js
@@ -4,11 +4,25 @@ angular.module('clique').controller('SetupController', ['$scope',
 	function($scope) {
 		// Setup
 		$scope.setup = function(){
-			// validate
-			// make sure the passwords match
-			if(this.password !== this.confirmPassword) {
-				
-			}
+			
 		}
 	}
 ]);
+
+angular.module('clique').directive('compareTo', function(){
+	return {
+		require: 'ngModel',
+		scope: {
+			otherModelValue: "=compareTo"
+		},
+		link: function(scope, element, attributes, ngModel) {
+			ngModel.$validators.compareTo = function(modelValue) {
+				return modelValue == scope.otherModelValue;
+			};
+
+			scope.$watch('otherModelValue', function() {
+				ngModel.$validate();
+			});
+		}
+	};
+});
